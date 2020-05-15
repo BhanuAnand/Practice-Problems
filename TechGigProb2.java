b5789c648d9572cde274377c0dbb35c5c6af1764
BhanuAnand <41252343+BhanuAnand@users.noreply.github.com>
2020-05-15 14:06:41 +0530

    Update and rename TGCG.java to TechGigProb2.java

--- TechGigProb2.java
+++ TechGigProb2.java
@@ -3,7 +3,7 @@
  
 import java.io.*;
 import java.util.*;
-public class CandidateCode {
+public class TechGigProb2 {
   public static void main(String args[] ) throws Exception {
      Scanner sc = new Scanner(System.in);
      String testCases = sc.nextLine();
