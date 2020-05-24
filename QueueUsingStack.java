a525bc62834c0453f31187820478d61238486beb
BhanuAnand <41252343+BhanuAnand@users.noreply.github.com>
2020-05-24 18:16:21 +0530

    Update QueueUsingStack.java
    comment added

--- QueueUsingStack.java
+++ QueueUsingStack.java
@@ -4,6 +4,7 @@ public class QueueUsingStack{
 	Stack<Integer> s1 = new Stack<>();
 	Stack<Integer> s2 = new Stack<>();
 
+// costly enqueue method implemented
     public void enqueue(int x){
     	while(!s1.isEmpty()){
     		s2.push(s1.pop());
